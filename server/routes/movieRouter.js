cbf0abcbe39d134f00edbd1cb2939bc4db68775c
Bùi Đoàn Quang Huy <85773586+huyanloi2002@users.noreply.github.com>
2024-06-06 17:25:25 +0700

    Update movieRouter.js

--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -15,5 +15,7 @@ router.put("/update_movie", updateMovie);
 router.patch("/like_movie/:id", likeMovie);
 router.get("/movie_by_id/:id", getMovieById);
 router.get("/movies_liked", getMoviesLiked);
+router.post("/add_details_movies", addDetailsMovies);
+
 
 module.exports = router;
