c37aa63a05225b5237c8e83c04d72cb4b1854507
Bùi Đoàn Quang Huy <85773586+huyanloi2002@users.noreply.github.com>
2024-06-18 08:12:28 +0000

    Update HomePage.jsx

--- client-v2/src/pages/HomePage.jsx
+++ client-v2/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TrendingMovie from "../components/TrendingMovie";
+import { MovieCardVertical } from "../components/MovieCard";
 
 const HomePage = () => {
   return (
@@ -8,10 +9,34 @@ const HomePage = () => {
         <div className="col-span-6 row-span-6 rounded-3xl bg-opacity-white-0.2 h-full w-full">
           <TrendingMovie />
         </div>
-        <div className="col-span-6 row-span-6 rounded-3xl bg-opacity-white-0.2"></div>
+        <div className="col-span-6 row-span-6 rounded-3xl bg-opacity-white-0.2">
+          <div className="h-full w-full flex flex-col items-center justify-center">
+            {/* <p className="text-md font-bold">Phim có thể bạn thích</p> */}
+            <div className="flex flex-wrap gap-3 justify-center">
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+              <MovieCardVertical />
+
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
 };
 
 export default HomePage;
+
